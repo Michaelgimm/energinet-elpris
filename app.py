1242542b9e6290c524b27504128d1d8eede07ded
Michael <michaelgimm@gmail.com>
2023-07-23 11:26:46 +0200

    style changed on submit button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import requests
 from datetime import datetime, timedelta
 from dateutil import parser
